bcdface427440581af45559a3080b94b824696fb
unadesuga <unauna@YOSUKEnoMacBook-puro.local>
2018-10-06 17:16:53 +0700

    create file

--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
 var app = new Vue({
-  el: '#app',
-  data: { //この３つはプロパティ
-    bpi: null,
-    hasError: false,
-    loading: true
-  },
-  mounted: function() { //axiosとはVue.jsのライブラリ
-    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(function(response) { //responseにAPIの戻り値が入っている
-        //console.log(response.data.bpi)
-        /* console.log(response.data.bpi.USD.rate_foat) */
-        this.bpi = response.data.bpi　　　 //bpiにresponse.data.bpiを入れる
-      }.bind(this)) //APIからbitcoin価格を取得しbpiプロパティにデータ入れる
-      .catch(function(error) {
-        console.log(error)
-        this.hasError = true //この行だけだとhasErrorにアクセスできないので、下のbind(this)要
-      }.bind(this))
-      .finally(function() { //このfinallyは通信が終わった最後に読まれる
-        this.loading = false
-      }.bind(this))
-  },
-  filters: {
-    currencyDecimal(value) {
-      return value.toFixed(2) //数を固定した小数点表記にする 小数点第２まで
+    el: '#app',
+    data: {
+      items: null,
+      keyword: '',
+      message: '',
+    },
+    watch: {
+
+    },
+    created: function() {    //mountedでも可能 DOMにアクセスしない場合createdの方が処理速度速い
+      this.keyword = 'JavaScript'
+      this.getAnswer()
+    },
+    methods: {
+      getAnswer: function() {
+        if(this.keyword === '') {
+           this.items = null
+           return
+        }
+
+        this.message = 'Loading....'
+        var vm = this
+                     //ページは１ページ表示 1pageあたり２０件表示 queryに検索ワードを渡す
+        var params = { page: 1, per_page: 20, query: this.keyword }
+        axios.get('https //qiita.com/api/v2/items', {params})
+          .then(function(response){
+             console.log(response)
+             vm.items = response.data
+          })
+          .catch(function(error) {
+            vm.message = 'Error!' + error
+          })
+          .finally(function() {
+             vm.message = ''
+          })
+      }
     }
-  }
 })
